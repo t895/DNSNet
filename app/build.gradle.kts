804523f15f35e47105abadd3d09977aa7cc17119
Charles Lombardo <clombardo169@gmail.com>
2025-03-04 13:24:44 -0500

    Bump version to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,8 +83,8 @@ android {
         applicationId = "dev.clombardo.dnsnet"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 35
-        versionName = "1.1.1"
+        versionCode = 36
+        versionName = "1.1.2"
 
         ndk {
             abiFilters += listOf("x86_64", "x86", "arm64-v8a", "armeabi-v7a")
