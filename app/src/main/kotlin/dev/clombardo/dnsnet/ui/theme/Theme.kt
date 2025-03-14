d49d413128c2f9a1a2fcf8a1a23f24aa09180a8c
Charles Lombardo <clombardo169@gmail.com>
2025-03-14 00:55:54 -0400

    Use global default LocalContentColor
    If a given surface does not provide a LocalContentColor, text and other content will appear using the light theme's onSurface color. This ensures that the color changes with the theme and is still overridden within child composables.

--- app/src/main/kotlin/dev/clombardo/dnsnet/ui/theme/Theme.kt
+++ app/src/main/kotlin/dev/clombardo/dnsnet/ui/theme/Theme.kt
@@ -10,12 +10,14 @@ package dev.clombardo.dnsnet.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
 
 private val lightScheme = lightColorScheme(
@@ -263,9 +265,11 @@ fun DnsNetTheme(
         else -> lightScheme
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = AppTypography,
-        content = content
-    )
+    CompositionLocalProvider(LocalContentColor provides colorScheme.onSurface) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = AppTypography,
+            content = content
+        )
+    }
 }
