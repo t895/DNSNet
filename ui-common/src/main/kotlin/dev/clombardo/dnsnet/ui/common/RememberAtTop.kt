cc5575f95f6d8fb0fedfb141ed9172404f89483d
Charles Lombardo <clombardo169@gmail.com>
2025-03-31 01:05:23 -0400

    ui-common: Helper function for remembering LazyListState::firstVisibleItemIndex

---
+++ ui-common/src/main/kotlin/dev/clombardo/dnsnet/ui/common/RememberAtTop.kt
@@ -0,0 +1,24 @@
+/* Copyright (C) 2025 Charles Lombardo <clombardo169@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+package dev.clombardo.dnsnet.ui.common
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+
+@Composable
+fun rememberAtTop(state: LazyListState): State<Boolean> {
+    return remember {
+        derivedStateOf {
+            state.firstVisibleItemIndex == 0
+        }
+    }
+}
