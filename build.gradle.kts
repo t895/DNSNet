d453b293d212b602a3b906a0aab66da96522d96a
Charles Lombardo <clombardo169@gmail.com>
2025-04-06 08:36:04 -0400

    Bump version to 1.1.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,5 +28,5 @@ plugins {
     alias(libs.plugins.gradle.play.publisher) apply false
 }
 
-val versionCode by extra { 50 }
-val versionName by extra { "1.1.16" }
+val versionCode by extra { 51 }
+val versionName by extra { "1.1.17" }
