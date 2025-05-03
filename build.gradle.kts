2ab06d0c9baadd526b7bbc5e454238798f361d4b
t895 <14132249+t895@users.noreply.github.com>
2025-05-03 05:52:45 +0000

    Bump version to 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,5 +27,5 @@ plugins {
     alias(libs.plugins.gradle.play.publisher) apply false
 }
 
-val versionCode by extra { 51 }
-val versionName by extra { "1.1.17" }
+val versionCode by extra { 52 }
+val versionName by extra { "1.2.0" }
