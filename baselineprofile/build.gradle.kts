7ea99e95cfd88c20824252eddaee16a2d7a6bedd
Charles Lombardo <clombardo169@gmail.com>
2025-06-01 21:31:24 -0400

    Use cached AVD for baseline profile generation

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 // You can specify to run the generators on a managed devices or connected devices.
 baselineProfile {
     managedDevices += "pixel6Api34"
-    useConnectedDevices = false
+    useConnectedDevices = true
 }
 
 dependencies {
