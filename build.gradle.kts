c5ed5e77cb749ddc296b9e8ae1dc93b41429cd6f
t895 <14132249+t895@users.noreply.github.com>
2025-07-06 16:19:59 +0000

    Bump version to 1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,5 +27,5 @@ plugins {
     alias(libs.plugins.gradle.play.publisher) apply false
 }
 
-val versionCode by extra { 59 }
-val versionName by extra { "1.3.0" }
+val versionCode by extra { 60 }
+val versionName by extra { "1.3.1" }
