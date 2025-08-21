55371fa66fe3625183dc7313d3b5e3e466f1d6e7
Charles Lombardo <clombardo169@gmail.com>
2025-08-20 22:29:56 -0400

    Add copyright notice to Settings.kt

--- settings/src/main/kotlin/dev/clombardo/dnsnet/settings/Settings.kt
+++ settings/src/main/kotlin/dev/clombardo/dnsnet/settings/Settings.kt
@@ -1,3 +1,11 @@
+/* Copyright (C) 2025 Charles Lombardo <clombardo169@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 package dev.clombardo.dnsnet.settings
 
 import android.content.Context
