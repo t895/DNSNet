00645b4f7679016fd09b2f96da1a5e512417c068
Charles Lombardo <clombardo169@gmail.com>
2025-12-21 23:31:56 -0500

    Revert SplitContentContainer color change
    This actually allowed the color to stay consistent as it animated to transparent

--- ui-common/src/main/kotlin/dev/clombardo/dnsnet/ui/common/SplitContentContainer.kt
+++ ui-common/src/main/kotlin/dev/clombardo/dnsnet/ui/common/SplitContentContainer.kt
@@ -145,7 +145,7 @@ fun SplitContentRowContainer(
         targetValue = if (elevated) {
             color
         } else {
-            Color.Transparent
+            color.copy(alpha = 0f)
         }
     )
     val contentColor by animateColorAsState(
@@ -277,7 +277,7 @@ fun SplitContentColumnContainer(
         targetValue = if (elevated) {
             color
         } else {
-            Color.Transparent
+            color.copy(alpha = 0f)
         }
     )
     val contentColor by animateColorAsState(
