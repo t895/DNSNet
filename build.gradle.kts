c9b7e42a66cf0104ad61e708394488a745618c16
t895 <14132249+t895@users.noreply.github.com>
2026-02-01 05:01:44 +0000

    Bump version to 1.3.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,5 +32,5 @@ plugins {
     alias(libs.plugins.dnsnet.atomicfu) apply false
 }
 
-val versionCode by extra { 68 }
-val versionName by extra { "1.3.7" }
+val versionCode by extra { 69 }
+val versionName by extra { "1.3.8" }
