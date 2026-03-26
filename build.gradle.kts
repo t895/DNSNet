27dcf51db8d38b8ad1b225fcd3a37bc157d17c66
t895 <14132249+t895@users.noreply.github.com>
2026-03-26 23:41:02 +0000

    Bump version to 1.3.15

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,5 +32,5 @@ plugins {
     alias(libs.plugins.dnsnet.atomicfu) apply false
 }
 
-val versionCode by extra { 75 }
-val versionName by extra { "1.3.14" }
+val versionCode by extra { 76 }
+val versionName by extra { "1.3.15" }
