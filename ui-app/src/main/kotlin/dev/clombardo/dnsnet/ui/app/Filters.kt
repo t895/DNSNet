bc0887e0d96db887a5aab7361652891fa11f7e7d
Charles Lombardo <clombardo169@gmail.com>
2026-09-08 21:01:52 -0400

    ui-app: Re-add dropdown menu import

--- ui-app/src/main/kotlin/dev/clombardo/dnsnet/ui/app/Filters.kt
+++ ui-app/src/main/kotlin/dev/clombardo/dnsnet/ui/app/Filters.kt
@@ -48,6 +48,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.ExposedDropdownMenu
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
